2e010f384ca1b4fc78ccb2ea50ff24ed72a6e0a8
Mithil Dani <danimithil@gmail.com>
2023-04-03 22:38:12 -0400

    added model to test foreign key migration, added a dummy command

---
+++ music/management/commands/testcommand.py
@@ -0,0 +1,6 @@
+from typing import Any, Optional
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Provides the basecommand as a command. Does nothing additional."
